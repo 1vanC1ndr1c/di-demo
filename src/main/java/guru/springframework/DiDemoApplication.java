92bf948a2c2e8074ef40723a9140f3820b668f30
Ivan Cindric <ivan.cindric95@gmail.com>
2018-10-06 23:18:42 +0200

    Rebuilt for java 8. Application doesn't work on java 10

--- src/main/java/guru/springframework/DiDemoApplication.java
+++ src/main/java/guru/springframework/DiDemoApplication.java
@@ -9,6 +9,7 @@ import org.springframework.context.ApplicationContext;
 public class DiDemoApplication {
 
 	public static void main(String[] args) {
+		//Ctrl+Alt+Shift+S and set to java 8. The app doesn't work on java 10
 		ApplicationContext ctx = SpringApplication.run(DiDemoApplication.class, args);
 
 		MyController controller = (MyController) ctx.getBean("myController");
