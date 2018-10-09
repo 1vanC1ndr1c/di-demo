060354b0905ca3a6e1b776f6743124fd99a8abb9
Ivan Cindric <ivan.cindric95@gmail.com>
2018-10-09 18:25:07 +0200

    Solved assigment. Fixed broken code

--- src/main/java/guru/springframework/services/PrimarySpanishGreetingService.java
+++ src/main/java/guru/springframework/services/PrimarySpanishGreetingService.java
@@ -1,5 +1,6 @@
 package guru.springframework.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
@@ -9,6 +10,13 @@ import org.springframework.stereotype.Service;
 @Primary//although there are 2 primary, it will be ignored because this profile is currently not active
 //to activate it -> application.properties spring.profiles.active=es
 public class PrimarySpanishGreetingService implements GreetingService {
+
+    private GreetingRepository greetingRepository;
+
+    @Autowired
+    public PrimarySpanishGreetingService(GreetingRepository greetingRepository) {
+        this.greetingRepository = greetingRepository;
+    }
 
     @Override
     public String sayGreeting() {
