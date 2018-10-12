2eb801bf3f5a790efe55134e2a680b71d0d81858
Ivan Cindric <ivan.cindric95@gmail.com>
2018-10-12 14:43:12 +0200

    Replace local annotations with all annotations in one place - GreetingService Config

--- src/main/java/guru/springframework/services/PrimaryGermanGreetingService.java
+++ src/main/java/guru/springframework/services/PrimaryGermanGreetingService.java
@@ -1,14 +1,17 @@
-package guru.services;
+package guru.springframework.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
+/*
 @Service
 @Profile("de")
 @Primary//although there are 3 primary now, it will be ignored because this profile is currently not active
 //to activate it -> application.properties spring.profiles.active=ge
+*/
+//better to use all annotations in one config file
 public class PrimaryGermanGreetingService implements GreetingService {
 
     private GreetingRepository greetingRepository;
--- src/main/java/guru/springframework/services/PrimarySpanishGreetingService.java
+++ src/main/java/guru/springframework/services/PrimarySpanishGreetingService.java
@@ -1,14 +1,17 @@
-package guru.services;
+package guru.springframework.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
+/*
 @Service
 @Profile("es")
 @Primary//although there are 2 primary, it will be ignored because this profile is currently not active
 //to activate it -> application.properties spring.profiles.active=es
+*/
+//better to use all annotations in one config file
 public class PrimarySpanishGreetingService implements GreetingService {
 
     private GreetingRepository greetingRepository;
