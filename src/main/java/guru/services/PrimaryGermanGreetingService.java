93fd55281a4fd65db48285b3eeded8f644cb5ab0
Ivan Cindric <ivan.cindric95@gmail.com>
2018-10-12 13:32:57 +0200

    Add Component Scan.

--- src/main/java/guru/services/PrimaryGermanGreetingService.java
+++ src/main/java/guru/services/PrimaryGermanGreetingService.java
@@ -1,7 +1,6 @@
-package guru.springframework.services;
+package guru.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
